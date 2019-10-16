a3c143289b01f5a10d6254d6d014b5a13ddb0f83
Walter-Feng <walterfeng@foxmail.com>
2019-10-16 11:50:35 +0100

    remove redundant files; add cmake

--- build/compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-source ~/.bashrc
-g++ ../src/*.cpp -Wall -I ${GSL_HOME}/include -c -g
-g++ *.o -L ${GSL_HOME}/lib -lgsl -lgslcblas -lm -g -o ../DVR
--- build/debug.sh
+++
@@ -1,2 +0,0 @@
-g++ -Wall -I /usr/local/include -g -c ../src/main.cpp ../src/FunctionLib.cpp ../src/fileprint.cpp
-g++ main.o FunctionLib.o fileprint.o -static -lgsl -lgslcblas -lm -g -o ../DVR
